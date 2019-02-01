ed7b8d72306c0b00e690030642d18a6c091f7c3b
NaughtyBox <47255511+NaughtyBox@users.noreply.github.com>
2019-02-01 17:52:38 -0200

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,5 @@
 def test():
+  """Somente um teste, n é necessário chamar a função"""
   subprocess.Popen(['calc.exe'], shell=True)
   with open('testupdator.txt', 'w') as fp:
     fp.write(os.getlogin())
-
-test()
